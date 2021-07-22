089e20c59f63b54f6ba48333c67c945a0b178cdc
teenastewart <teenastewart@gmail.com>
2021-07-21 21:54:23 -0500

    fixing code for markers. Adding legend.

--- static/js/logic.js
+++ static/js/logic.js
@@ -7,19 +7,82 @@ d3.json(queryUrl).then(function(data) {
     console.log(data)
     // Run createFeatures function with data
     createFeatures(data.features);
-  
+
 });
 
 function createFeatures(earthquakeData) {
 
+
+    function getColor(coordinates) {
+      // for (var i = 0; i < earthquakeData.length; i++) {
+
+        // Conditionals for countries points
+        var color = "";
+        if (coordinates[2] >= 90) {
+          color = "#154360";
+        }
+        else if (coordinates[2] <= 89 && coordinates[2] >= 70) {
+          color = "#1F618D";
+        }
+        else if (coordinates[2] <= 69 && coordinates[2] >= 50) {
+          color = "#2980B9";
+        }
+        else if (coordinates[2] <= 49 && coordinates[2] >= 30) {
+            color = "#5499C7";
+          }
+          else if (coordinates[2] <= 29 && coordinates[2] >= 10) {
+            color = "#7FB3D5";
+          }
+        else {
+          color = "#D4E6F1";
+        }
+       
+        return (color);
+        // switch(coordinates[2]) {
+        // case (coordinates[2] >= 90) :
+        //   return "blue";
+        
+        // case (coordinates[2] <= 89 && coordinates[2] >= 70) :
+        //   return "green";
+        
+        // case (coordinates[2] <= 69 && coordinates[2] >= 50) :
+        //   return "yellow";
+        
+        // case (coordinates[2] <= 49 && coordinates[2] >= 30) :
+        //   return "orange";
+          
+        // case (coordinates[2] <= 29 && coordinates[2] >= 10) :
+        //     return "red";
+          
+        // case (coordinates[2] < 10):
+        //   return "pink";
+        
+       
+        }
+
+    // }
+  
+    
+    function getRadius(mag) {
+      var radius = Math.floor(mag) * 7
+      return radius
+    }
+
     //Run a function for each feature to create the pop-up 
     function onEachFeature(feature, layer) {
       layer.bindPopup("<h3>" + feature.properties.place +
-        "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+        "</h3><hr><p>" + new Date(feature.properties.time) + "</p><p>" + "Magnitude: "+ feature.properties.mag + "</p><p>" + "Depth: "+ feature.geometry.coordinates[2] + "</p>");
     }
   
     // Create the geoJSON layer
     var earthquakes = L.geoJSON(earthquakeData, {
+      pointToLayer: function (feature, latlng) {
+        return new L.CircleMarker(latlng, {radius: getRadius(feature.properties.mag), 
+                                            fillOpacity: 1, 
+                                            color: 'black', 
+                                            fillColor: getColor(feature.geometry.coordinates), 
+                                            weight: 1,});
+        },
       onEachFeature: onEachFeature,
 
     });
@@ -60,6 +123,24 @@ function createMap(earthquakes) {
     layers: [lightmap, earthquakes]
   });
 
+  /*Legend specific*/
+var legend = L.control({ position: "bottomleft" });
+
+legend.onAdd = function(map) {
+  var div = L.DomUtil.create("div", "legend");
+  div.innerHTML += "<h4>Earthquake Depths</h4>";
+  div.innerHTML += '<i style="background: #D4E6F1"></i><span>-10 - 10</span><br>';
+  div.innerHTML += '<i style="background: #7FB3D5"></i><span>10 - 30</span><br>';
+  div.innerHTML += '<i style="background: #5499C7"></i><span>30 - 50</span><br>';
+  div.innerHTML += '<i style="background: #2980B9"></i><span>50 - 70</span><br>';
+  div.innerHTML += '<i style="background: #1F618D"></i><span>70 - 90</span><br>';
+  div.innerHTML += '<i style="background: #154360"></i><span>90+</span><br>';
+
+  return div;
+};
+
+legend.addTo(myMap);
+
 
   // Create a layer control
   // Pass in our baseMaps and overlayMaps
@@ -68,3 +149,4 @@ function createMap(earthquakes) {
     collapsed: false
   }).addTo(myMap);
 }
+
